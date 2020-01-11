64103dfdf5422ceaa6c5b07857bb8ecd1c2c9ade
tkoteswararao131997 <shafatkaloo8@gmail.com>
2020-01-11 10:32:59 +0530

    [SHAFAT].Powerof2 added with full implementation

--- Bridgelabz/src/main/java/com/bridgelabz/basicprogram/PowerOF2.java
+++ Bridgelabz/src/main/java/com/bridgelabz/basicprogram/PowerOF2.java
@@ -1,5 +1,35 @@
+/************************************************************************************************************************
+ * purpose :  takes a command-line argument N and prints a table of the powers of 2 that are less than or equal to 2^N **
+ *                                                                                                                     **   
+ * @author SHAFAT BASHIR                                                                                               **
+ * @version 1.0                                                                                                        **
+ * @since 10/01/2020                                                                                                   **
+ * **********************************************************************************************************************        
+ */
 package com.bridgelabz.basicprogram;
 
+import com.bridgelabz.util.Utility;
+
 public class PowerOF2 {
+
+	public static void main(String[] args) {
+
+		Utility.output("Enter the Value of N : ");
+		String s = Utility.scannerString();
+		int n;
+		if (Utility.numberOrNot(s)) {
+			n = Utility.numberOrNotReturn(s);
+			if (n < 31) {
+				for (int i = 0; i <= n; i++) {
+			        Utility.output(Utility.StringOf(Math.pow(2, i)));
+				}
+			} else {
+				Utility.output("! Invalid input value ,datatype int  can't it");
+			}
+		}
+
+	}
+
+
 
 }
--- Bridgelabz/src/main/java/com/bridgelabz/util/Utility.java
+++ Bridgelabz/src/main/java/com/bridgelabz/util/Utility.java
@@ -1,17 +1,19 @@
 package com.bridgelabz.util;
 
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Utility {
-    
+
 
      // Get Input from the user     
 	public static String scannerString() {
 		Scanner input = new Scanner(System.in);
 		return input.next();
 	}
-	
-	
+
+
 	// Validating the Input FOR UserInput
 	public static String validatingInputforUserInput() {
 		 String input;
@@ -21,24 +23,24 @@ public class Utility {
 			 return null;
 		  }
 	}
-	
+
 	// Random Number generator for coin flip
 	public static double rNumberGenerator() {
 		return Math.random();
 	}
-	
+
 	//Takes  User input -n-  
 	public static int scannerInt() {
 		Scanner input = new Scanner(System.in);
 		input.hasNext();
 		return input.nextInt();
 	}
-	
+
 	//Takes two arguments X and Y gives us Percentage
 	public static double percentage(double x,double y) {
 		return (x / y) * 100;
 	}
-	
+
 	//Whether Year is a Leap year or not
 	public static boolean leapOrNot(int year) {
 		boolean flag = false;
@@ -53,6 +55,48 @@ public class Utility {
 		}
 		return flag;
 	}
-	
-	//
+
+	//Returns Power of X^y  in integer
+	public static int powerof(int x,int y) {
+	 return (int) Math.pow(x, y);	
+	}
+
+	//Returns Power of X^y  in double
+	public static double powerof(double x,double y) {
+	 return  Math.pow(x, y);	
+	}
+
+	//Validates if input is Integer 
+	public static boolean numberOrNot(String input){
+		try
+		{
+			Integer.parseInt(input);
+		}
+		catch(NumberFormatException ex) {
+			return false;
+		}
+		return true;
+	}
+
+	//if true returns Integer 
+	public static int numberOrNotReturn(String input){
+		return Integer.parseInt(input);
+	}
+
+	//Logger to log output 
+	public static void output(String value) {
+		Logger logger = Logger.getLogger(Utility.class.getName());
+		logger.log(Level.INFO,value);
+	}
+
+	//Converts double into String
+	public static String StringOf(double value) {
+		return String.valueOf(value);		
+	}
+	//Converts int into String
+	public static String StringOf(int value) {
+		return String.valueOf(value);	
+	}
+
+
 }
