3022bce08d07712984309979ee719f464370e073
tkoteswararao131997 <shafatkaloo8@gmail.com>
2020-01-11 10:15:43 +0530

    [SHAFAT].Refractor few improments in userinput.java

--- Bridgelabz/src/main/java/com/bridgelabz/basicprogram/UserInput.java
+++ Bridgelabz/src/main/java/com/bridgelabz/basicprogram/UserInput.java
@@ -1,8 +1,10 @@
-/******************************************
- * purpose : To take  User Input and Replace String Template “Hello <<UserName>>, How are you?”  
+/*************************************************************************************************
+ * purpose : To take  User Input and Replace String Template “Hello <<UserName>>, How are you?” 
+ * 
  * @author SHAFAT BASHIR
  * @version 1.0
  * @since 09/01/2020
+ * ***********************************************************************************************
  */
 
 package com.bridgelabz.basicprogram;
@@ -14,15 +16,14 @@ import com.bridgelabz.util.Utility;
 public class UserInput {
 
 	public static void main(String[] args) {
-		
+		String result;
 	  Logger logger = Logger.getLogger(UserInput.class.getName());
-	  String userName;
-	  if(((userName = Utility.scannerString() ) != null) && userName.length() >= 3) {
-		  logger.log(Level.INFO, userName);
+	  System.out.println("Enter the input : ");
+	  if((result = Utility.validatingInputforUserInput()) != null) {
+		  logger.log(Level.INFO,"Hello <<"+result+">>, How are you?");
 	  }else {
-		  logger.log(Level.WARNING, "! Invalid UserName ");
+		  logger.log(Level.WARNING,"! Invalid Input");
 	  }
-	  
 	}
 
 }
