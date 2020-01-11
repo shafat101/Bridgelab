a2b4aea6d2ff00d3d41b5bd6534ff8075a3abb8d
tkoteswararao131997 <shafatkaloo8@gmail.com>
2020-01-11 10:10:41 +0530

    [SHAFAT].Refractor userinput.java modified takes input from user,validates,uses utility class

--- Bridgelabz/src/main/java/com/bridgelabz/basicprogram/UserInput.java
+++ Bridgelabz/src/main/java/com/bridgelabz/basicprogram/UserInput.java
@@ -1,18 +1,28 @@
-package com.bridgelabz.basicprogram;
+/******************************************
+ * purpose : To take  User Input and Replace String Template “Hello <<UserName>>, How are you?”  
+ * @author SHAFAT BASHIR
+ * @version 1.0
+ * @since 09/01/2020
+ */
 
-import java.util.Scanner;
+package com.bridgelabz.basicprogram;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.bridgelabz.util.Utility;
 
 public class UserInput {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		System.out.println("Enter the UserName : ");
-		String userName = input.next();
 		
+	  Logger logger = Logger.getLogger(UserInput.class.getName());
+	  String userName;
+	  if(((userName = Utility.scannerString() ) != null) && userName.length() >= 3) {
+		  logger.log(Level.INFO, userName);
+	  }else {
+		  logger.log(Level.WARNING, "! Invalid UserName ");
+	  }
 	  
-	  System.out.println("Hello <<" + userName + ">>, How are you?");
-	  
-
 	}
 
 }
